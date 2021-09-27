e7d4cc7540e9e28293f1f15b9a13358ac7e130f3
Julien Vanharen <julien.vanharen@gmail.com>
2021-09-27 21:28:05 +0200

    conda env

--- dot_envs/freyja.zsh
+++ dot_envs/freyja.zsh
@@ -6,14 +6,14 @@ export PATH=$WKDIR/bin:$PATH
 
 # Conda configuration.
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/jvanhare/desktop/wkdir/miniconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/Users/jvanhare/desktop/wkdir/conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/Users/jvanhare/desktop/wkdir/miniconda/etc/profile.d/conda.sh" ]; then
-        . "/Users/jvanhare/desktop/wkdir/miniconda/etc/profile.d/conda.sh"
+    if [ -f "/Users/jvanhare/desktop/wkdir/conda/etc/profile.d/conda.sh" ]; then
+        . "/Users/jvanhare/desktop/wkdir/conda/etc/profile.d/conda.sh"
     else
-        export PATH="/Users/jvanhare/desktop/wkdir/miniconda/bin:$PATH"
+        export PATH="/Users/jvanhare/desktop/wkdir/conda/bin:$PATH"
     fi
 fi
 unset __conda_setup
