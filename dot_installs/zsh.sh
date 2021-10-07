2339dd9d4d6a497824b73d10fc577a3933a74919
Julien Vanharen <julien.vanharen@gmail.com>
2021-10-07 18:07:58 +0200

    Add dot_installs/zsh.sh

---
+++ dot_installs/zsh.sh
@@ -0,0 +1,12 @@
+### Install ZSH. ###
+cd $SRC
+if [[ -d "zsh" ]]
+then
+      rm -fr zsh
+    fi
+    git clone https://github.com/zsh-users/zsh.git
+    cd zsh
+    git checkout zsh-5.8
+    ./Util/preconfig
+    ./configure --prefix=$WKDIR/spiro
+    make -j48 install
