4d49aaca7ba1da58329f47c007c231f5b767194f
Julien Vanharen <julien.vanharen@gmail.com>
2021-10-07 18:38:37 +0200

    zsh install

--- dot_envs/spiro.zsh
+++ dot_envs/spiro.zsh
@@ -7,4 +7,21 @@ source $HOME/.envs/onera.zsh
 export PATH=$WKDIR/spiro/bin:$PATH
 export LD_LIBRARY_PATH=$WKDIR/spiro/lib:$LD_LIBRARY_PATH
 
+# ZSH install script. ###
+install_zsh() {
+    cd $SRC
+    if [[ -d "zsh" ]]
+    then
+        rm -fr zsh
+    fi
+    git clone https://github.com/zsh-users/zsh.git
+    cd zsh
+    git checkout zsh-5.8
+    ./Util/preconfig
+    ./configure --prefix=$WKDIR/spiro
+    make -j48 install
+    cd ..
+    rm -fr zsh
+}
+
 # EOF.
--- run_permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp $HOME/.installs/src/* $HOME/.installs/
-chmod u+x $HOME/.installs/*
-
-# EOF.
