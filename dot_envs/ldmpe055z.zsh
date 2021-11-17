c05ee9029136477457bbab134aae78493fde985d
Julien Vanharen <julien.vanharen@gmail.com>
2021-11-17 14:25:49 +0100

    ldmpe055z

--- dot_envs/ldmpe055z.zsh
+++ dot_envs/ldmpe055z.zsh
@@ -4,6 +4,7 @@ source $HOME/.envs/onera.zsh
 # Path configuration. ##########################################################
 export PATH=$WKDIR/ldmpe055z/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
+export PATH=$WKDIR/ldmpe055z/bin/vizir4_centos.2021.07.06:$PATH
 export LD_LIBRARY_PATH=$WKDIR/ldmpe055z/lib:$LD_LIBRARY_PATH
 # Aliases. #####################################################################
 alias code="/opt/tools/vscode/1.60.0/bin/vscode.sh"
