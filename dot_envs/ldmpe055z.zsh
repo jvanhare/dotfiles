1329f9d42d8bfb903da64644a2677976dfc89ffb
Julien Vanharen <julien.vanharen@gmail.com>
2021-11-18 11:34:58 +0100

    ldmpe055z

--- dot_envs/ldmpe055z.zsh
+++ dot_envs/ldmpe055z.zsh
@@ -6,6 +6,8 @@ export PATH=$WKDIR/ldmpe055z/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$WKDIR/ldmpe055z/bin/vizir4_centos.2021.07.06:$PATH
 export LD_LIBRARY_PATH=$WKDIR/ldmpe055z/lib:$LD_LIBRARY_PATH
+# Source spack. ################################################################
+test -d $SRC/spack && source $SRC/spack/share/spack/setup-env.sh
 # Aliases. #####################################################################
 alias code="/opt/tools/vscode/1.60.0/bin/vscode.sh"
 # EOF. #########################################################################
