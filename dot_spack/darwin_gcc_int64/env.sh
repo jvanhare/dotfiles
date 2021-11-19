4f177b6e126fbb3959da436ee404a7bf21d37ca4
Julien Vanharen <julien.vanharen@gmail.com>
2021-11-20 00:04:16 +0100

    Update dot_spack/darwin_gcc_int64/env.sh

--- dot_spack/darwin_gcc_int64/env.sh
+++ dot_spack/darwin_gcc_int64/env.sh
@@ -1,2 +1,7 @@
 module purge
 source $HOME/.spack/darwin_gcc_int64/loads
+export CC=/usr/local/bin/gcc-11
+export CXX=/usr/local/bin/g++-11
+export F77=/usr/local/bin/gfortran-11
+export FC=/usr/local/bin/gfortran-11
+
