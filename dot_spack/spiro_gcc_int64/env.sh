5661a12b5fcbb7537a440e93caf5dc2c7f967b19
Julien Vanharen <julien.vanharen@gmail.com>
2021-12-15 16:18:11 +0100

    rename

--- dot_spack/spiro_gcc_int64/env.sh
+++
@@ -1,4 +0,0 @@
-module purge
-test -d /home/jvanhare/wkdir/src/spack && source /home/jvanhare/wkdir/src/spack/share/spack/setup-env.sh
-source /home/jvanhare/.spack/spiro_gcc_int64/loads
-module load gcc/8.3
