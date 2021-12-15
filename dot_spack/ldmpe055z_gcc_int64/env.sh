a99d8171f3f1922504559ef2b2a56b3b2ae5c977
Julien Vanharen <julien.vanharen@gmail.com>
2021-12-15 16:25:29 +0100

    Update dot_spack/ldmpe055z_gcc_int64/env.sh

--- dot_spack/ldmpe055z_gcc_int64/env.sh
+++ dot_spack/ldmpe055z_gcc_int64/env.sh
@@ -1,6 +1,5 @@
 module purge
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-source $SCRIPT_DIR/loads
+source $HOME/.spack/ldmpe055z_gcc_int64/loads
 
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
