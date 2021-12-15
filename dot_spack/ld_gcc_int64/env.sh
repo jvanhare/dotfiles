69e2de3bb2a5da055d68fb310e6d967760b540dd
Julien Vanharen <julien.vanharen@gmail.com>
2021-12-15 15:49:59 +0100

    Update dot_spack/ld_gcc_int64/env.sh Update dot_spack/ld_gcc_int64/loads Update dot_spack/ld_gcc_int64/spack.lock Update dot_spack/ld_gcc_int64/spack.yaml

--- dot_spack/ld_gcc_int64/env.sh
+++ dot_spack/ld_gcc_int64/env.sh
@@ -1,6 +1,7 @@
 module purge
-module load openmpi/4.0.5-gnu831
 source $HOME/.spack/ld_gcc_int64/loads
+# module load openmpi/4.0.5-gnu831
+
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 export F77=/usr/bin/gfortran
