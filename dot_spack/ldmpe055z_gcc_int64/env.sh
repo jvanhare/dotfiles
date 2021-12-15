9add68b798d35ddd3c40b1bec2e58dbb880d642b
Julien Vanharen <julien.vanharen@gmail.com>
2021-12-15 16:23:42 +0100

    Update dot_spack/ldmpe055z_gcc_int64/env.sh

--- dot_spack/ldmpe055z_gcc_int64/env.sh
+++ dot_spack/ldmpe055z_gcc_int64/env.sh
@@ -2,7 +2,9 @@ module purge
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 source $SCRIPT_DIR/loads
 
-module load gcc/8.3
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+export F77=/usr/bin/gfortran
+export FC=/usr/bin/gfortran
 
 # EOF.
-
