96df19bd4768b9a851b6e4a470e43f1845efef7c
Julien Vanharen <julien.vanharen@gmail.com>
2021-12-15 16:19:37 +0100

    Add dot_spack/spiro_gcc_int64/env.sh

---
+++ dot_spack/spiro_gcc_int64/env.sh
@@ -0,0 +1,4 @@
+module purge
+test -d /home/jvanhare/wkdir/src/spack && source /home/jvanhare/wkdir/src/spack/share/spack/setup-env.sh
+source /home/jvanhare/.spack/spiro_gcc_int64/loads
+module load gcc/8.3
