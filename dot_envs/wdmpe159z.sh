515e13bd732192e3ae31c44461385977e4f6cced
Julien Vanharen <julien.vanharen@gmail.com>
2021-12-17 10:43:15 +0100

    wdmpe159z

---
+++ dot_envs/wdmpe159z.sh
@@ -0,0 +1,22 @@
+# Set WSL Ubuntu's environment. ################################################
+# Working directory and path. ##################################################
+export WKDIR=$HOME/wkdir
+export PATH=$WKDIR/bin:$PATH
+export SRC=$WKDIR/src
+# Aliases. #####################################################################
+alias ls="ls -ltrGh"
+# Source poetry and install completions. #######################################
+# export PATH=$HOME/Library/Python/3.9/bin:$PATH
+# test ! -d $HOME/.zfunc && mkdir $HOME/.zfunc
+# poetry completions zsh > $HOME/.zfunc/_poetry
+# fpath+=~/.zfunc
+# Source spack and lmod. #######################################################
+# test -d $SRC/spack && source $SRC/spack/share/spack/setup-env.sh
+# source /usr/local/opt/lmod/init/zsh
+# MPI configuration. ###########################################################
+export PMIX_MCA_gds=hash
+export OMPI_MCA_btl=vader,self
+# Address sanitizer and memory leaks detection. ################################
+export ASAN_OPTIONS="detect_leaks=1"
+export LSAN_OPTIONS="suppressions=$HOME/.lsan:print_suppressions=false"
+# EOF. #########################################################################
