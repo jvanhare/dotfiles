f1eb52cfbc4e5fc59bd71b1d554ca094a0dc77de
Julien Vanharen <julien.vanharen@gmail.com>
2022-01-11 14:17:23 +0100

    freyja

--- dot_spack/freyja_gcc_int64/env.sh
+++
@@ -1,9 +0,0 @@
-module purge
-source $HOME/.spack/freyja_gcc_int64/loads
-
-export CC=/usr/local/bin/gcc-11
-export CXX=/usr/local/bin/g++-11
-export F77=/usr/local/bin/gfortran-11
-export FC=/usr/local/bin/gfortran-11
-
-# EOF.
