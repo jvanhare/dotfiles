c0cd43b6942c82b8b69c65a9d6e8611b08a64f66
Julien Vanharen <julien.vanharen@gmail.com>
2022-02-09 01:46:13 +0100

    ldmpe

--- dot_envs/ldmpe055z.sh
+++ dot_envs/ldmpe055z.sh
@@ -9,4 +9,8 @@ export LD_LIBRARY_PATH=$WKDIR/ldmpe055z/lib:$LD_LIBRARY_PATH
 test -d $SRC/spack && source $SRC/spack/share/spack/setup-env.sh
 # Aliases. #####################################################################
 alias code="/opt/tools/vscode/1.60.0/bin/vscode.sh"
+# Address sanitizer and memory leaks detection. ################################
+export ASAN_OPTIONS="detect_leaks=1"
+export LSAN_OPTIONS="suppressions=$HOME/.lsan:print_suppressions=false"
+export LSAN_OPTIONS="$LSAN_OPTIONS:fast_unwind_on_malloc=false"
 # EOF. #########################################################################
