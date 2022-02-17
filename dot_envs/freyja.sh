84f110cc2a93f67a47d3202d679b6f9ab7ec903a
Julien Vanharen <julien.vanharen@gmail.com>
2022-02-17 18:34:28 +0100

    freyja

--- dot_envs/freyja.sh
+++ dot_envs/freyja.sh
@@ -5,21 +5,8 @@ export PATH=$WKDIR/bin:$PATH
 export PATH=/Applications/vizir.app/Contents/MacOS:$PATH
 export PATH=/Applications/vizir4.app/Contents/MacOS:$PATH
 export SRC=$WKDIR/src
-# Source the system's library for Intel compilers. #############################
-export MACOSX_DEPLOYMENT_TARGET=11.5
-export MACOSX_SDK=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr
-export CXXFLAGS=-I$MACOSX_SDK/include
-export CFLAGS=-I$MACOSX_SDK/include
-export LIBRARY_PATH=$MACOSX_SDK/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$MACOSX_SDK/lib:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=$MACOSX_SDK/lib:$DYLD_LIBRARY_PATH
 # Aliases. #####################################################################
 alias ls="ls -ltrGh"
-# Source poetry and install completions. #######################################
-export PATH=$HOME/Library/Python/3.9/bin:$PATH
-test ! -d $HOME/.zfunc && mkdir $HOME/.zfunc
-poetry completions zsh > $HOME/.zfunc/_poetry
-fpath+=~/.zfunc
 # Source spack and lmod. #######################################################
 test -d $SRC/spack && source $SRC/spack/share/spack/setup-env.sh
 source /usr/local/opt/lmod/init/sh
@@ -29,4 +16,6 @@ export OMPI_MCA_btl=vader,self
 # Address sanitizer and memory leaks detection. ################################
 export ASAN_OPTIONS="detect_leaks=1"
 export LSAN_OPTIONS="suppressions=$HOME/.lsan:print_suppressions=false"
+# Add llvm to path. ############################################################
+export PATH="/usr/local/opt/llvm/bin:$PATH"
 # EOF. #########################################################################
