df69380361d50b0be8fe28093c2290ce9c66e10c
Julien Vanharen <julien.vanharen@gmail.com>
2022-06-20 20:03:39 +0200

    Cleaning

--- dot_envs/freyja.sh
+++
@@ -1,8 +0,0 @@
-# Set freyja's environment. ####################################################
-{{ template "export.darwin.tmpl" . }}
-# Aliases. #####################################################################
-alias ls="ls -ltrGh"
-# Source spack and lmod. #######################################################
-test -d $SRC/spack && source $SRC/spack/share/spack/setup-env.sh
-source /usr/local/opt/lmod/init/sh
-# EOF. #########################################################################
