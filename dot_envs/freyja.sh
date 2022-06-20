2f50279a32146611840b1b91b0b41ee06a63091c
Julien Vanharen <julien.vanharen@gmail.com>
2022-06-20 19:59:50 +0200

    Cleaning

--- dot_envs/freyja.sh
+++ dot_envs/freyja.sh
@@ -1,25 +1,8 @@
 # Set freyja's environment. ####################################################
-# Working directory and path. ##################################################
-export WKDIR=$HOME/Desktop/wkdir
-export PATH=$WKDIR/bin:$PATH
-export PATH=/Applications/vizir.app/Contents/MacOS:$PATH
-export PATH=/Applications/vizir4.app/Contents/MacOS:$PATH
-export SRC=$WKDIR/src
+{{ template "export.darwin.tmpl" . }}
 # Aliases. #####################################################################
 alias ls="ls -ltrGh"
 # Source spack and lmod. #######################################################
 test -d $SRC/spack && source $SRC/spack/share/spack/setup-env.sh
 source /usr/local/opt/lmod/init/sh
-# MPI configuration. ###########################################################
-export PMIX_MCA_gds=hash
-export OMPI_MCA_btl=vader,self
-# Address sanitizer and memory leaks detection. ################################
-export ASAN_OPTIONS="detect_leaks=1"
-export LSAN_OPTIONS="suppressions=$HOME/.lsan:print_suppressions=false"
-# Add llvm to path. ############################################################
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-# Configure ruby. ##############################################################
-source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
-source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
-chruby ruby-3.1.2
 # EOF. #########################################################################
