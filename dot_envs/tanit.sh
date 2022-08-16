b16b2692b9c7b1feddcf8881c5f731072bd8ce42
Julien Vanharen <julien.vanharen@gmail.com>
2022-08-16 13:28:52 +0200

    Update

--- dot_envs/tanit.sh
+++
@@ -1,7 +0,0 @@
-# Set tanit's environment. #####################################################
-# Get ONERA's environment. #####################################################
-source $HOME/.envs/onera.sh
-# Path configuration. ##########################################################
-export PATH=$WKDIR/tanit/bin:$PATH
-export LD_LIBRARY_PATH=$WKDIR/tanit/lib:$LD_LIBRARY_PATH
-# EOF. #########################################################################
