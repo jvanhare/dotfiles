971f3aa374a164aef007522b9214100010727865
Julien Vanharen <julien.vanharen@inria.fr>
2024-01-29 17:10:49 +0100

    Update

--- dot_hpc/tmplfiles/module.lua
+++ dot_hpc/tmplfiles/module.lua
@@ -6,6 +6,15 @@ setenv("CC", "{{ (datasource "packages").env.cc }}")
 setenv("CXX", "{{ (datasource "packages").env.cxx }}")
 setenv("HPC_ENVIRONMENT", "{{ .Env.PWD | filepath.Base }}")
 
+setenv("PMIX_MCA_gds", "hash")
+setenv("OMPI_MCA_btl", "vader,self")
+
+setenv("CTEST_PARALLEL_LEVEL", "8")
+
+setenv("MallocNanoZone", "0")
+setenv("ASAN_OPTIONS", '"detect_leaks=1"')
+setenv("LSAN_OPTIONS", '"suppressions=$HOME/.lsan:print_suppressions=false"')
+
 prepend_path("CMAKE_PREFIX_PATH", "{{ .Env.PWD }}")
 prepend_path("LD_LIBRARY_PATH", "{{ .Env.PWD }}/lib")
 prepend_path("DYLD_LIBRARY_PATH", "{{ .Env.PWD }}/lib")
